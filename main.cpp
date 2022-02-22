4ec1539ccf6c48ffbb663b72eceff61ef01d729c
Sergioooooooooo <94106409+Sergioooooooooo@users.noreply.github.com>
2022-02-22 13:43:17 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct
+
+int main(int argc, char *argv[]) {
+	
+	string color_cartas [40];
+	int i;
+	int j=0;
+	int n=1,
+	for (i=0;i<10;i++) {
+	
+	color_cartas [j]="verde";
+		
+		
+		
+		cout << "La carta " << color_cartas [j] <<"\n" ;
+		j++;
+}
+	int numero_carta[40]
+	
+	
+	
+	return 0;
+}
+
